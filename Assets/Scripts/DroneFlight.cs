18478883f3c94c147047963f3512373a7a50a945
valdeezzee <jvalderrama91@hotmail.com>
2015-01-29 18:04:59 -0500

    Drone Unity Project

---
+++ Assets/Scripts/DroneFlight.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneFlight : MonoBehaviour {
+
+
+	private GameObject[] clockWisePropeller;
+	private GameObject[] counterClockWisePropeller;
+	private Transform[] propellerRotation;
+
+	private float clockWiseRoation;
+	private float counterClockWiseRotation;
+
+	public NavMeshAgent agent;
+	public GameObject target;
+	public Vector3 targetPostion;
+	// Use this for initialization
+	void Start () 
+	{
+
+		targetPostion = target.transform.position;
+		//gets the propellers
+		clockWisePropeller = GameObject.FindGameObjectsWithTag("ClockwisePropeller");
+		counterClockWisePropeller = GameObject.FindGameObjectsWithTag("CounterclockPropeller");
+		//propeller rotation speeds
+		clockWiseRoation = 20;
+		counterClockWiseRotation = -20;
+		agent = GetComponent<NavMeshAgent>();
+		//agent.SetDestination(targetPostion);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//loops and rotates individual propeller
+		for(int i = 0 ; i < clockWisePropeller.Length; i++)
+		{
+			clockWisePropeller[i].transform.Rotate(0, clockWiseRoation, 0, 0);
+			counterClockWisePropeller[i].transform.Rotate(0, counterClockWiseRotation, 0, 0);
+		}
+
+
+		//Debug.Log(clockWiseRoation);
+
+
+	}
+
+	public void setDestination(Vector3 destination)
+	{
+
+		agent.SetDestination(destination);
+
+
+	}
+
+
+}
---
+++ Assets/Scripts/Fly_To.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly_To : MonoBehaviour {
+
+	public GameObject Drone;
+	public DroneFlight droneDestination;
+	// Use this for initialization
+	void Start () {
+		//gets the drone GameObject and gets the DroneFlight script
+		Drone = GameObject.Find("Drone");
+		droneDestination = Drone.GetComponent<DroneFlight>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButtonDown(0))
+		{
+			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if(Physics.Raycast(ray, out hit))
+			{
+				if(hit.transform.gameObject.tag == "Building")
+				{
+					print (camera.transform.position + "HERE");
+				//this to be the drone destination 
+				//Sends the position you want to send your drone to
+				print(hit.transform.position);
+				droneDestination.setDestination(hit.transform.position);
+				}
+			}
+		}
+	}
+}
