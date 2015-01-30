924f99ea19b6c070f66a5c218879978c3a02df6b
valdeezzee <jvalderrama91@hotmail.com>
2015-01-29 21:37:50 -0500

    Added more Drone
    it doesn't work

--- Assets/Scripts/Fly_To.cs
+++ Assets/Scripts/Fly_To.cs
@@ -5,17 +5,20 @@ public class Fly_To : MonoBehaviour {
 
 	public GameObject Drone;
 	public DroneFlight droneDestination;
+
+ 
 	// Use this for initialization
 	void Start () {
 		//gets the drone GameObject and gets the DroneFlight script
 		Drone = GameObject.Find("Drone");
 		droneDestination = Drone.GetComponent<DroneFlight>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if(Input.GetMouseButtonDown(0))
+		if(Input.GetMouseButtonDown(0) && this.gameObject.tag == "MiniMap")
 		{
 			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
@@ -24,13 +27,24 @@ public class Fly_To : MonoBehaviour {
 			{
 				if(hit.transform.gameObject.tag == "Building")
 				{
-					print (camera.transform.position + "HERE");
+				print("new drone");
+				//print (camera.transform.position + "HERE");
 				//this to be the drone destination 
 				//Sends the position you want to send your drone to
-				print(hit.transform.position);
+				//print(hit.transform.position);
 				droneDestination.setDestination(hit.transform.position);
 				}
 			}
 		}
+
+	}
+
+	public void dronePossessed(GameObject newDrone)
+	{
+
+		Drone = newDrone;
+		droneDestination = Drone.GetComponent<DroneFlight>();
+
+
 	}
 }
--- Assets/Scripts/PossessDrone.cs
+++ Assets/Scripts/PossessDrone.cs
@@ -5,35 +5,44 @@ public class PossessDrone : MonoBehaviour {
 
 	public GameObject Drone;
 	public DroneFlight droneDestination;
+	public GameObject miniMap;
+	public Fly_To newDrone;
 	// Use this for initialization
 	void Start () {
 		//gets the drone GameObject and gets the DroneFlight script
 		Drone = GameObject.Find("Drone");
 		droneDestination = Drone.GetComponent<DroneFlight>();
+		miniMap = GameObject.Find("MiniMap");
+		newDrone = miniMap.GetComponent<Fly_To>();
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if(Input.GetMouseButtonDown(0))
-		{
+		{	/*
+			if (Input.GetButtonDown("Fire1") && Camera.main.pixelRect.Contains(Input.mousePosition))
+
+           */
 			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 			
 			if(Physics.Raycast(ray, out hit))
-			{
-				if(hit.transform.gameObject.tag == "Building")
-				{
-					print (camera.transform.position + "HERE");
-					/*
-					use this to get the parent of the camera you click on and posses that drone!
+			{ 		
+				 //if(hit.transform.gameObject.tag == "Building"){
+					
+
+					// use this to get the parent of the camera you click on and posses that drone!
 					Drone = this.transform.parent.gameObject;
-					*/
+					newDrone.dronePossessed(Drone);
+				    print ("CLICKed");
+				    print(camera.transform.position + "HERE");
 					//this to be the drone destination 
 					//Sends the position you want to send your drone to
-					print(hit.transform.position);
-					droneDestination.setDestination(hit.transform.position);
-				}
+					//print(hit.transform.position);
+					//droneDestination.setDestination(hit.transform.position);
+				//}
 			}
 		}
 	}
