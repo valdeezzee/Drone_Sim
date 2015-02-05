e1315f5dfc9a5831f1becdd9b55d695fd1cc3558
davecurtisc <davidcurtis@knights.ucf.edu>
2015-02-05 14:10:41 -0500

    Fuel and Compass Canvas Updates

---
+++ Assets/Sliderscale.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Sliderscale: MonoBehaviour {
+	public Image FuelBar;
+	public float DroneFuel;
+	private float maxFuel = 1;
+	// Use this for initialization
+	void Start () {
+		DroneFuel = maxFuel;
+		FuelBar = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FuelBar.fillAmount = DroneFuel;
+		//FuelBar.transform.localScale 
+	}
+}
